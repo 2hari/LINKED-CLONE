4552e75dc463a871b478a3e0c15850040a8ee497
Hari <two1821125run@protonmail.com>
2022-03-05 19:19:51 +0530

    sidebar session
    useSession vs getSession both are used on same page , how useSession is used to get status vs to just authenticate. Same sidebar component vanishing its parts in mobile screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
 
-
-function MyApp({ Component, pageProps }) {
+// spreading session like that allows us to access session in each pages prop instead of having to pass it manually each time
+function MyApp({ Component, pageProps:{session, ...pageProps} }) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   )
 }
 
